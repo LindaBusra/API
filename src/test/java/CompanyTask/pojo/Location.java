ceb6ef60dfb2b6e0bd50324f75a63c98f0c29471
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-06-16 22:27:42 +0200

    Task

---
+++ src/test/java/CompanyTask/pojo/Location.java
@@ -0,0 +1,53 @@
+package CompanyTask.pojo;
+
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.annotate.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Location {
+
+    //create variables
+    @JsonProperty("latitude")
+    private String latitude;
+    @JsonProperty("longitude")
+    private String longitude;
+
+
+    public Location() {
+    }
+
+    public Location(String latitude, String longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+
+    @JsonProperty("latitude")
+    public String getLatitude() {
+        return latitude;
+    }
+
+    @JsonProperty("latitude")
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    @JsonProperty("longitude")
+    public String getLongitude() {
+        return longitude;
+    }
+
+    @JsonProperty("longitude")
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "latitude='" + latitude + '\'' +
+                ", longitude='" + longitude + '\'' +
+                '}';
+    }
+}
